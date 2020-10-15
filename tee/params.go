36c6382cfee44b780f555d6239d57d87b2cdfe1c
Sebastian Stammler <seb@perun.network>
2020-10-15 18:01:17 +0200

    tee: Add TEE address and response duration to Parameters

--- tee/params.go
+++ tee/params.go
@@ -5,10 +5,12 @@ package tee
 import "github.com/ethereum/go-ethereum/common"
 
 type Parameters struct {
-	PowDepth      uint64         // required confirmed block depth
-	PhaseDuration uint64         // number of blocks of one phase (not epoch)
-	InitBlock     uint64         // block at which Erdstall contract was deployed
-	Contract      common.Address // Erdstall contract address
+	PowDepth         uint64         // required confirmed block depth
+	PhaseDuration    uint64         // number of blocks of one phase (not epoch)
+	ResponseDuration uint64         // challenge response grace period for operator at end of exit phase
+	InitBlock        uint64         // block at which Erdstall contract was deployed
+	TEE              common.Address // Enclave's public key address
+	Contract         common.Address // Erdstall contract address
 }
 
 // DepositEpoch returns the deposit epoch at the given block number.
@@ -34,4 +36,8 @@ func (p Parameters) FreezingEpoch(params Parameters, blockNum uint64) Epoch {
 // Don't use this, use the specific FooEpoch methods.
 func (p Parameters) epoch(blockNum uint64) Epoch {
 	return (blockNum - p.InitBlock) / p.PhaseDuration
+}
+
+func (p Parameters) IsChallengeResponsePhase(blockNum uint64) bool {
+	return p.PhaseDuration-((blockNum-p.InitBlock)%p.PhaseDuration) <= p.ResponseDuration
 }
