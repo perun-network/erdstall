1afcca38f624aa28e6aa2250bc3004711f480469
Sebastian Stammler <seb@perun.network>
2020-10-19 18:19:44 +0200

    eth: export DefaultGasLimit

--- eth/client.go
+++ eth/client.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	defaultGasLimit = 2000000
+	DefaultGasLimit = 2000000
 	defaultPowDepth = 0
 )
 
@@ -183,7 +183,7 @@ func (cl *Client) DeployContracts(params *tee.Parameters) error {
 	ctx, cancel := NewDefaultContext()
 	defer cancel()
 
-	tr, err := cl.NewTransactor(ctx, big.NewInt(0), defaultGasLimit, cl.account)
+	tr, err := cl.NewTransactor(ctx, big.NewInt(0), DefaultGasLimit, cl.account)
 	if err != nil {
 		return fmt.Errorf("creating transactor: %w", err)
 	}
