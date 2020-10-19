f16de53a1fb04481ff817fe2a60b18e88827f21d
Sebastian Stammler <seb@perun.network>
2020-10-19 16:28:14 +0200

    tee/prototype: quit block processor on quit signal

--- tee/prototype/blockproc.go
+++ tee/prototype/blockproc.go
@@ -12,26 +12,29 @@ func (e *Enclave) blockProcessor(
 	newBlocks <-chan *tee.Block,
 	verifiedBlocks chan<- *tee.Block,
 ) error {
-	// read blocks from newBlocks
 	k := e.params.PowDepth
-	for b := range newBlocks {
-		if e.bc.Len() == 0 && e.params.InitBlock != b.NumberU64() {
-			return fmt.Errorf("first block (%d) not initial Erdstall block(%d)", b.NumberU64(), e.params.InitBlock)
-		}
+	for {
+		select {
+		case b := <-newBlocks:
+			if e.bc.Len() == 0 && e.params.InitBlock != b.NumberU64() {
+				return fmt.Errorf("first block (%d) not initial Erdstall block (%d)", b.NumberU64(), e.params.InitBlock)
+			}
 
-		// verify block chain
-		if err := e.bc.PushVerify(b); err != nil {
-			return fmt.Errorf("pushing block to local blockchain: %w", err)
-		}
+			// verify block chain
+			if err := e.bc.PushVerify(b); err != nil {
+				return fmt.Errorf("pushing block to local blockchain: %w", err)
+			}
 
-		// TODO: * Handle Reorgs...
-		//   * Receipts verification
+			// TODO: * Handle Reorgs...
+			//   * Receipts verification
 
-		// write verified block (up to PoW-depth) to verifiedBlocks
-		l := e.bc.Len()
-		if l > k {
-			verifiedBlocks <- e.bc.blocks[l-k]
+			// write verified block (up to PoW-depth) to verifiedBlocks
+			l := e.bc.Len()
+			if l > k {
+				verifiedBlocks <- e.bc.blocks[l-k]
+			}
+		case <-e.quit:
+			return nil
 		}
 	}
-	return nil
 }
