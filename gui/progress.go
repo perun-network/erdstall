477f13de38403844c117ac30d1aeb57b7b5b4bb8
Oliver Tale-Yazdi <oliver@perun.network>
2020-10-23 03:12:08 +0200

    :sparkles: [gui] Add GUI.
    Signed-off-by: Oliver Tale-Yazdi <oliver@perun.network>

---
+++ gui/progress.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package gui
+
+import (
+	"strings"
+	"sync"
+)
+
+type ProgressBar struct {
+	Title string
+	// < 0 means unknown
+	Width    int
+	Status   []string
+	finished bool
+
+	mtx *sync.Mutex
+}
+
+func (p *ProgressBar) Add(status string) {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+	if p.finished {
+		return
+	}
+	p.Status = append(p.Status, "├ "+status)
+}
+
+func (p *ProgressBar) Finish(status string) {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+	if p.finished {
+		return
+	}
+	p.Status = append(p.Status, "└─"+status)
+	p.finished = true
+}
+
+func (p *ProgressBar) Render() string {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+	return "┌─" + p.Title + "\n" + strings.Join(p.Status, "\n")
+}
