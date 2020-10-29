96407a9b57f25040679a961f19ce59931fd994bc
Matthias Geihs <matthias@perun.network>
2020-10-29 10:43:32 +0100

    [cmd/operator] Add flag log-level

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -10,13 +10,20 @@ import (
 
 func main() {
 	configFilePath := flag.String("config", "config.json", "config file path")
+	logLevel := flag.String("log-level", "info", "log level")
 	flag.Parse()
 
 	cfg, err := operator.LoadConfig(*configFilePath)
 	operator.AssertNoError(err)
 	log.Println("Config loaded")
 
-	_operator, _ := operator.Setup(cfg)
+	lvl, err := log.ParseLevel(*logLevel)
+	if err != nil {
+		log.Fatalf("parsing log level: %v", err)
+	}
+	log.SetLevel(lvl)
+
+	_operator := operator.Setup(cfg)
 	err = _operator.Serve(cfg.Port)
 	operator.AssertNoError(err)
 }
