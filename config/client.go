84ec213cea17a8338efcaccb066e22452037b81c
Steffen Rattay <steffen@perun.network>
2021-01-14 18:28:50 +0100

    config: Added chain URL dictionary to client cfg.
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- config/client.go
+++ config/client.go
@@ -2,10 +2,15 @@
 
 package config
 
-import "flag"
+import (
+	"encoding/json"
+	"flag"
+
+	log "github.com/sirupsen/logrus"
+)
 
 type ClientConfig struct {
-	ChainURL     string
+	ChainURLs    map[string]string // map[ChainID]url
 	OpHost       string
 	OpPort       int
 	Mnemonic     string
@@ -15,7 +20,8 @@ type ClientConfig struct {
 }
 
 func ParseClientConfig() (cfg ClientConfig) {
-	flag.StringVar(&cfg.ChainURL, "chain-url", "ws://127.0.0.1:8545", "'protocol://ip:port'")
+	var urlsJson string
+	flag.StringVar(&urlsJson, "chain-urls", `{"1337": "ws://127.0.0.1:8545"}`, `JSON dictionary {"chainID": Ethereum node URL}`)
 	flag.StringVar(&cfg.OpHost, "op-host", "127.0.0.1", "IP/host name of operator")
 	flag.IntVar(&cfg.OpPort, "op-port", 8401, "Port of operator.")
 	flag.StringVar(&cfg.Mnemonic, "mnemonic", "pistol kiwi shrug future ozone ostrich match remove crucial oblige cream critic", "Wallet mnemonic.")
@@ -23,5 +29,36 @@ func ParseClientConfig() (cfg ClientConfig) {
 	flag.StringVar(&cfg.Contract, "contract", "", "Address of the Erdstall contract. Empty means deploy.")
 	flag.StringVar(&cfg.UserName, "username", "<anonymous>", "Set an optional username.")
 	flag.Parse()
+
+	cfg.ChainURLs = parseChainURLs(urlsJson)
+
 	return
+}
+
+func parseChainURLs(jsonText string) map[string]string {
+	m := make(map[string]string)
+	var urls map[string]interface{}
+	if err := json.Unmarshal([]byte(jsonText), &urls); err != nil {
+		log.WithError(err).Error("Unmarshal chain URL")
+	}
+
+	for k, v := range urls {
+		vs, ok := v.(string)
+		if !ok {
+			log.Panicf("Client config: chain URLs: expected string, got `%v`", v)
+		}
+		m[k] = vs
+	}
+	return m
+}
+
+// ChainURL looks up the configured ethereum network URL for a network ID.
+func (c *ClientConfig) ChainURL(networkID string) string {
+	chURL, ok := c.ChainURLs[networkID]
+	if !ok {
+		log.WithField("network ID", networkID).
+			Fatal("Unknown ethereum network ID")
+	}
+
+	return chURL
 }
