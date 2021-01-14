b38fe3e0c911da92b08bd607856b6cd9896d6b2d
Steffen Rattay <steffen@perun.network>
2021-01-14 18:31:05 +0100

    config: Fixed config description.
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- config/client.go
+++ config/client.go
@@ -16,8 +16,8 @@ type ClientConfig struct {
 
 func ParseClientConfig() (cfg ClientConfig) {
 	flag.StringVar(&cfg.ChainURL, "chain-url", "ws://127.0.0.1:8545", "'protocol://ip:port'")
-	flag.StringVar(&cfg.OpHost, "op-host", "127.0.0.1", "IP of hostname")
-	flag.IntVar(&cfg.OpPort, "op-port", 8401, "Account derivation index.")
+	flag.StringVar(&cfg.OpHost, "op-host", "127.0.0.1", "IP/host name of operator")
+	flag.IntVar(&cfg.OpPort, "op-port", 8401, "Port of operator.")
 	flag.StringVar(&cfg.Mnemonic, "mnemonic", "pistol kiwi shrug future ozone ostrich match remove crucial oblige cream critic", "Wallet mnemonic.")
 	flag.IntVar(&cfg.AccountIndex, "account-index", 0, "Account derivation index.")
 	flag.StringVar(&cfg.Contract, "contract", "", "Address of the Erdstall contract. Empty means deploy.")
