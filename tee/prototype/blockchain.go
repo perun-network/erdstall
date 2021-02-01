36c104492ab827ffb1a058d8782db7f279941abd
Steffen Rattay <steffen@perun.network>
2021-02-01 13:48:41 +0100

    tee/prototype: Simplified blockchain.
    - Threw out fork handling.
    - Only tracking the latest block now.
    - Adapted blockchain test.
    - Now validates block receipts before pushing a block, so that no errors
      can occur after pushing a block.
    
    Signed-off-by: Steffen Rattay <steffen@perun.network>

--- tee/prototype/blockchain.go
+++ tee/prototype/blockchain.go
@@ -5,84 +5,142 @@ package prototype
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+
 	"github.com/perun-network/erdstall/tee"
 )
 
+// Blockchain keeps track of the blockchain and ensures the integrity of new
+// blocks entered in to the enclave.
 type blockchain struct {
-	offset uint64 // block.number of first block in blocks slice
-	blocks []*tee.Block
+	head *tee.Block
 }
 
-func (b *blockchain) Head() *tee.Block {
-	if len(b.blocks) == 0 {
-		return nil
-	}
-	return b.blocks[len(b.blocks)-1]
-}
-
-// Block returns the block with the given block number. Panics if not present.
-func (b *blockchain) Block(blockNum uint64) *tee.Block {
-	return b.blocks[blockNum-b.offset]
-}
-
-func (b *blockchain) Len() uint64 {
-	return uint64(len(b.blocks))
-}
+// Head returns the latest verified and processed block or nil.
+func (b *blockchain) Head() *tee.Block { return b.head }
+func (b *blockchain) empty() bool      { return b.head == nil }
 
 // PushVerify pushes the block onto the chain, verifying that it is indeed a
-// correct next block.
-//
-// If the chain's current head has the same or a larger block number, a chain
-// reorg is assumed and the current block becomes the new head, discarding all
-// previous blocks.
-func (b *blockchain) PushVerify(block *tee.Block) error {
-	blockNum := block.NumberU64()
-	if len(b.blocks) == 0 {
-		// first block
-		b.offset = blockNum
-		b.blocks = []*tee.Block{block}
-		return nil
+// valid successor block of the previous head. If the block is valid, returns
+// all of the block's deposit and exit events. If the block is invalid, the
+// chain remains unchanged.
+func (b *blockchain) PushVerify(
+	block *tee.Block,
+	params *tee.Parameters,
+	epoch *Epoch,
+) ([]*erdstallDepEvent, []*erdstallExitEvent, error) {
+	if !b.empty() {
+		if err := verifySuccessorBlock(block, b.head); err != nil {
+			return nil, nil, fmt.Errorf("verifying successor block: %v", err)
+		}
 	}
 
-	prev := b.Head()
-	prevNum := prev.NumberU64()
-	if blockNum > prevNum+1 {
-		return fmt.Errorf("intermediate blocks missing, head: %d, block: %d", prevNum, blockNum)
-	} else if blockNum <= prevNum {
-		// reorg
-		prev = b.Block(blockNum - 1)
-		prevNum = prev.NumberU64()
+	deps, exits, err := extractEvents(block, params)
+	if err != nil {
+		return nil, nil, fmt.Errorf("invalid block events: %w", err)
 	}
 
-	if err := verifyBlock(block, prev); err != nil {
-		return fmt.Errorf("verifying block: %v", err)
+	blockN := block.NumberU64()
+	depEpoch, exitEpoch := params.DepositEpoch(blockN), params.ExitEpoch(blockN)
+
+	if err = verifyDeposits(deps, depEpoch); err != nil {
+		return nil, nil, fmt.Errorf("invalid deposits: %w", err)
+	} else if err = verifyExits(exits, exitEpoch, epoch); err != nil {
+		return nil, nil, fmt.Errorf("invalid exits: %w", err)
 	}
 
-	b.blocks = append(b.blocks[:prevNum+1-b.offset], block)
+	b.head = block
+	return deps, exits, err
+}
+
+// verifySuccessorBlock verifies that next is a valid successor of head.
+func verifySuccessorBlock(next, head *tee.Block) error {
+	nextHash, headHash := next.Header().ParentHash, head.Hash()
+	nextN, headN := next.NumberU64(), head.NumberU64()
+
+	// TODO: Extend validation to test consensus (PoW).
+	//   This function then probably becomes a method on nextchain after we add a
+	//   consensus engine to nextchain.
+	switch {
+	case nextN != headN+1:
+		return fmt.Errorf("next is not successor, head: %d, next: %d", headN, nextN)
+	case nextHash != headHash:
+		return fmt.Errorf("head header mismatch, expected %x, got: %x", nextHash, headHash)
+	}
 	return nil
 }
 
-// PruneUntil can be used to discard all blocks until the given block number
-func (b *blockchain) PruneUntil(blockNum uint64) {
-	if b.offset >= blockNum {
-		return
-	}
+// extractEvents extracts all deposit and exit events from a block.
+func extractEvents(
+	block *tee.Block,
+	params *tee.Parameters,
+) (deps []*erdstallDepEvent, exits []*erdstallExitEvent, _ error) {
+	predicates := []logPredicate{logIsDepositEvt, logIsExitEvt}
 
-	diff := blockNum - b.offset
-	b.blocks = b.blocks[diff:len(b.blocks)]
-	b.offset = blockNum
+	for _, r := range block.Receipts {
+		logs := filterLogs(r.Logs, predicates, params.Contract)
+		depLogs, exitLogs := logs[0], logs[1]
+		for _, depLog := range depLogs {
+			if dep, err := parseDepEvent(depLog); err != nil {
+				return nil, nil, fmt.Errorf("failed to parse deposit: %w", err)
+			} else {
+				deps = append(deps, dep)
+			}
+		}
+		for _, exitLog := range exitLogs {
+			if exit, err := parseExitEvent(exitLog); err != nil {
+				return nil, nil, fmt.Errorf("failed to parse exit: %w", err)
+			} else {
+				exits = append(exits, exit)
+			}
+		}
+	}
+	return
 }
 
-// verifyBlock verifies that block is a valid next block after parent.
-func verifyBlock(block, parent *tee.Block) error {
-	bHash := block.Header().ParentHash
-	pHash := parent.Hash()
+// filterLogs partitions logs into different buckets of matching predicates.
+// Only logs from the Erdstall contract are filtered and other logs, as well as
+// those without a matching predicate, are discarded.
+func filterLogs(logs []*types.Log, preds []logPredicate, contract common.Address) [][]*types.Log {
+	buckets := make([][]*types.Log, len(preds))
+	for _, l := range logs {
+		if l.Address != contract {
+			// only parse Erdstall logs
+			continue
+		}
+		for i, p := range preds {
+			if p(l) {
+				buckets[i] = append(buckets[i], l)
+			}
+		}
+	}
+	return buckets
+}
 
-	// TODO: Extend validation to test consensus (PoW).
-	//   This function then probably becomes a method on blockchain after we add a
-	//   consensus engine to blockchain.
-	if bHash != pHash {
-		return fmt.Errorf("parent header mismatch, expected %x, got: %x", pHash, bHash)
+func verifyDeposits(
+	deposits []*erdstallDepEvent,
+	depEpoch tee.Epoch,
+) error {
+	for i, dep := range deposits {
+		if dep.Epoch != depEpoch {
+			return fmt.Errorf("invalid epoch %d != %d in deposit[%d]", dep.Epoch, depEpoch, i)
+		}
+	}
+	return nil
+}
+
+func verifyExits(
+	exits []*erdstallExitEvent,
+	exitEpoch tee.Epoch,
+	epoch *Epoch,
+) error {
+	for i, exit := range exits {
+		if exit.Epoch != exitEpoch {
+			return fmt.Errorf("invalid epoch %d != %d in exit[%d]", exit.Epoch, exitEpoch, i)
+		} else if exit.Value.Cmp(epoch.Balance(exit.Account)) != 0 {
+			return fmt.Errorf("balance mismatch in exit[%d]", i)
+		} // TODO: assert frozen.
 	}
 	return nil
 }
--- tee/prototype/blockchain_internal_test.go
+++ tee/prototype/blockchain_internal_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/perun-network/erdstall/tee"
@@ -21,31 +22,67 @@ func TestBlockchain(t *testing.T) {
 			require.NoError(err)
 			return &tee.Block{Block: *b}
 		}
-		bc blockchain
+		bc     blockchain
+		params = &tee.Parameters{PhaseDuration: 10}
+		epoch  = newEpoch(0)
 	)
 
-	ethbc.Commit()
-	require.NoError(bc.PushVerify(head()))
+	t.Run("empty chain", func(t *testing.T) {
+		require.True(bc.empty())
+		require.Nil(bc.Head())
+	})
+
+	t.Run("origin block", func(t *testing.T) {
+		ethbc.Commit()
+		deps, exits, err := bc.PushVerify(head(), params, epoch)
+		require.Empty(deps)
+		require.Empty(exits)
+		require.NoError(err)
+		require.Equal(bc.Head(), head())
+	})
 
 	ethbc.Commit()
 	b1 := head()
-	require.NoError(bc.PushVerify(b1))
-	require.NoError(bc.PushVerify(b1))
-	require.Len(bc.blocks, 2)
+
+	t.Run("normal successor", func(t *testing.T) {
+		deps, exits, err := bc.PushVerify(b1, params, epoch)
+		require.Empty(deps)
+		require.Empty(exits)
+		require.NoError(err)
+	})
+
+	t.Run("duplicate block", func(t *testing.T) {
+		_, _, err := bc.PushVerify(b1, params, epoch)
+		require.Error(err)
+	})
 
 	ethbc.Commit()
-	b2 := head()
-	require.NoError(bc.PushVerify(b2))
-	require.Len(bc.blocks, 3)
-	require.NoError(bc.PushVerify(b1))
-	require.Len(bc.blocks, 2, "pushing previous block should shorten bc")
-	require.Equal(bc.Head(), b1)
 
+	t.Run("Invalid parent hash", func(t *testing.T) {
+		h2 := head().Header()
+		h2.ParentHash[0] ^= 0x3f
+		b2 := &tee.Block{Block: *types.NewBlockWithHeader(h2)}
+		_, _, err := bc.PushVerify(b2, params, epoch)
+		require.Error(err)
+		require.Equal(bc.Head(), b1)
+	})
+
+	b2 := head()
 	ethbc.Commit()
 	b3 := head()
-	require.Error(bc.PushVerify(b3))
-	require.NoError(bc.PushVerify(b2))
-	require.NoError(bc.PushVerify(b3))
-	require.Len(bc.blocks, 4)
-	require.Equal(bc.Head(), b3)
+
+	t.Run("block gap", func(t *testing.T) {
+		_, _, err := bc.PushVerify(b3, params, epoch)
+		require.Error(err)
+		require.Equal(bc.Head(), b1)
+		_, _, err = bc.PushVerify(b2, params, epoch)
+		require.NoError(err)
+		require.Equal(bc.Head(), b2)
+	})
+
+	t.Run("past block", func(t *testing.T) {
+		_, _, err := bc.PushVerify(b1, params, epoch)
+		require.Error(err)
+		require.Equal(bc.Head(), b2)
+	})
 }
